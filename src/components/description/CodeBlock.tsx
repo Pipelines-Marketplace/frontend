7e62c81e6c6a7fa6e029be0b58a78af40489eaaa
Puneet Punamiya <Puneet@localhost.localdomain>
2019-11-26 15:08:20 +0530

    Changed the YAML file editor theme

--- src/components/description/CodeBlock.tsx
+++ src/components/description/CodeBlock.tsx
@@ -3,12 +3,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
-import {atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+// import {atomDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 const CodeBlock = (props:any) => {
   const {value} = props;
   return (
-    <SyntaxHighlighter language="yaml" style={atomDark} showLineNumbers={true} wrapLines={true}>
+    <SyntaxHighlighter language="yaml" showLineNumbers={true} wrapLines={true}>
       {value}
     </SyntaxHighlighter>
   );
