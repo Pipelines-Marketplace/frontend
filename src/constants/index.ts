da245439ad8914a407e88794efae37833c11d7fc
Chetan Banavikalmutt <chetanrns1997@gmail.com>
2019-12-09 15:52:13 +0530

    Added backend service name

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export const ENTER_KEY = 13;
-export const API_URL = 'http://backend-marketplace-demo.apps.cluster-blr-a552.blr-a552.example.opentlc.com';
+// export const API_URL = 'http://backend-marketplace-demo.apps.cluster-blr-a552.blr-a552.example.opentlc.com';
+export const API_URL= 'http://backend:5000'
