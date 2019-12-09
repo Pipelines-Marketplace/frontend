327ffafe918c4bbc98cc8a7490b65a3d4f7cf661
Chetan Banavikalmutt <chetanrns1997@gmail.com>
2019-12-09 16:02:19 +0530

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
 export const ENTER_KEY = 13;
-// export const API_URL = 'http://backend-marketplace-demo.apps.cluster-blr-a552.blr-a552.example.opentlc.com';
-export const API_URL= 'http://backend:5000'
+export const API_URL = 'http://backend-marketplace-demo.apps.cluster-blr-a552.blr-a552.example.opentlc.com';
