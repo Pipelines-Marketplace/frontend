08e3321a1fce4a2e7cfbb930533470e58b81c3bb
chetan-rns <chetanrns1997@gmail.com>
2019-12-17 11:12:32 +0530

    Deleted unused files

--- src/constants/index.tsx
+++
@@ -1,2 +0,0 @@
-export const ENTER_KEY = 13;
-export const API_URL='http://backend-marketplace-test.apps.cluster-blr-8849.blr-8849.example.opentlc.com';
--- src/redux/actions/types.tsx
+++
@@ -1,2 +0,0 @@
-export const FETCH_TASKS='FETCH_TASKS';
-
