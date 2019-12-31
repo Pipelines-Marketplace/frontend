654f8ebab7c36a57e843193dfd162a74812c9fd7
Puneet Punamiya <puneetpunamiya@gmail.com>
2019-12-31 11:44:16 +0530

    Added process.env to access the environemnt variable

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,4 @@
 export const ENTER_KEY = 13;
-export const API_URL = 'http://backend-marketplace-test.apps.cluster-blr-24f3.blr-24f3.example.opentlc.com';
+// export const API_URL = 'http://backend-marketplace-test.apps.cluster-blr-24f3.blr-24f3.example.opentlc.com';
+export const API_URL = process.env.REACT_APP_API_URL;
+
