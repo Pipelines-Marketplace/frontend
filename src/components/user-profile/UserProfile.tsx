2db7c19a75c7c83366418a71416adf9b1cab2c39
Puneet Punamiya <puneetpunamiya@gmail.com>
2020-01-03 12:03:06 +0530

    Updated user profile in terms of deleting a resource

--- src/components/user-profile/UserProfile.tsx
+++ src/components/user-profile/UserProfile.tsx
@@ -1,47 +1,34 @@
 import React from 'react';
-import {Card,
-  CardHead,
-  TextContent,
-  CardActions,
-  Flex,
-  FlexItem,
-  Button,
-} from '@patternfly/react-core';
-import {OkIcon, Remove2Icon, DownloadIcon} from '@patternfly/react-icons';
-import imgAvatar from '../assets/logo/imgAvatar.png';
+import {API_URL} from '../../constants';
+import UserProfileChild from './UserProfileChild';
 
 const UserProfile: React.FC = (props: any) => {
+  const userGithubId = localStorage['usetrID'];
+  const [userResource, setUserResource] = React.useState();
+  React.useEffect(() => {
+    fetch(`${API_URL}/resources/user/${userGithubId}`)
+        .then((response) => response.json())
+        .then((data: any) => setUserResource(data));
+  // eslint-disable-next-line
+  }, []);
+
+
+  if (userResource !== undefined) {
+    if (userResource.length > 0) {
+      return (
+        <div>
+          {
+            <UserProfileChild task = {userResource} />
+
+          }
+
+        </div>
+      );
+    }
+  };
+
   return (
-    <Card style={{marginLeft: '13em', marginRight: '7em',
-      marginTop: '2em', width: '100%', padding: '0'}}>
-      <CardHead>
-        <img src ={imgAvatar} alt="Task"
-          style={{height: '3em', marginLeft: '2em'}}
-        />
-
-        <Flex breakpointMods={[{modifier: 'column', breakpoint: 'lg'}]}>
-          <FlexItem>
-            <TextContent style={{marginLeft: '3em', marginTop: '0.5em'}}>
-          TaskName
-            </TextContent>
-          </FlexItem>
-
-        </Flex>
-
-        <CardActions style={{marginRight: '5em'}}>
-          <DownloadIcon style = {{marginRight: '0.2em'}} className="download"/>
-          <TextContent className="text">10</TextContent>
-          <OkIcon style={{color: 'green'}}/>
-          <TextContent className="text">4.5</TextContent>
-          <Button variant="danger" style = {{marginLeft: '3em'}}>Delete
-            <Remove2Icon style = {{marginLeft: '1em', marginTop: '0.3em'}}/>
-          </Button>
-        </CardActions>
-      </CardHead>
-
-
-    </Card>
-
+    <div></div>
   );
 };
 
